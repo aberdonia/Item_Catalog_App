237c7e29136cfa309f31c05383deac8326efb2ac
jamo92 <32820619+jamo92@users.noreply.github.com>
2018-02-13 19:52:23 +0000

    Update and rename myapp.wsgi to project.py

--- project.py
+++ project.py
@@ -1,6 +1,3 @@
-activate_this = '/path/to/env/bin/activate_this.py'
-execfile(activate_this, dict(__file__=activate_this))
-
 from flask import Flask, render_template, request, redirect, url_for, flash
 
 
